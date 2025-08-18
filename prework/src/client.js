f0da250cb3257ff88279c5d830d93226f7908584
Muhammed-Abdulbasit <muhammedabdulbasit2654@gmail.com>
2025-08-18 17:37:05 -0400

    Initial setup

---
+++ prework/src/client.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'https://swjenskcpjbfmzopypjr.supabase.co';
+const API_KEY = 'sb_publishable_AiZ-69xYJyo38Uo9FAY4yw_aCHgKYwN';
+const supabase = createClient(URL, API_KEY)
+export const supabase = createClient(URL, API_KEY);
